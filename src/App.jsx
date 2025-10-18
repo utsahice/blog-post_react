3cdfb74aab15011e87c99b7ba26f55ce7ac8b4c1
utsahice <utsahic.encircle@gmail.com>
2025-10-18 15:35:02 +0530

    feat: .prettierrc edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import MyButton from './components/button'
-import BlogPostList from './components/bloglist'
+import React from "react";
+import MyButton from "./components/button";
+import BlogPostList from "./components/bloglist";
 function App() {
-
   return (
     <>
-     <h1 className="text-3xl font-bold text-center italic tracking-wider text-blue-400 uppercase font-serif ">
-      Blog Post
-    </h1>
-    <BlogPostList />
+      <h1 className="text-3xl font-bold text-center italic tracking-wider text-blue-400 uppercase font-serif ">
+        Blog Post
+      </h1>
+      <BlogPostList />
     </>
-  
-  )
+  );
 }
 
-export default App
+export default App;
